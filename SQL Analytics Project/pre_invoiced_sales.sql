131af87695c09704e77380fad4c670df8877ede2
ANMOL SINGH <anmolzzabc100@gmail.com>
2025-07-19 02:22:30 +0530

    modified pre_invoice_sales added cte

--- SQL Analytics Project/pre_invoiced_sales.sql
+++ SQL Analytics Project/pre_invoiced_sales.sql
@@ -17,4 +17,32 @@
             ON pre.customer_code = s.customer_code AND
             pre.fiscal_year=s.fiscal_year
 	WHERE 
+	s.fiscal_year=2021    
+    
+    ##cte 
+    
+    with cte as (	SELECT 
+    	   s.date, 
+           s.product_code, 
+           p.product, 
+	   p.variant, 
+           s.sold_quantity, 
+           g.gross_price as gross_price_per_item,
+           ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+           pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_product p              
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    	    ON g.fiscal_year=s.fiscal_year
+    	    AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+            ON pre.customer_code = s.customer_code AND
+            pre.fiscal_year=s.fiscal_year
+	WHERE 
 	s.fiscal_year=2021     
+)
+   
+    select *,round((gross_price_total * (1-pre_invoice_discount_pct)),2) as net_inoice_sales from cte c
+    limit 50
+    
