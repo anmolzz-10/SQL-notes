f4727ad65456b8a77ed8ec06de84ca31af56e34c
ANMOL SINGH <anmolzzabc100@gmail.com>
2025-07-18 19:56:38 +0530

    query added, views directory added

---
+++ SQL Analytics Project/pre_invoiced_sales.sql
@@ -0,0 +1,20 @@
+	SELECT 
+    	   s.date, 
+           s.product_code, 
+           p.product, 
+	   p.variant, 
+           s.sold_quantity, 
+           g.gross_price as gross_price_per_item,
+           ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+           pre.pre_invoice_discount_pct, round((s.sold_quantity*g.gross_price *(1-pre.pre_invoice_discount_pct)),2) as net_invoice_sales
+	FROM fact_sales_monthly s
+	JOIN dim_product p              
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    	    ON g.fiscal_year=s.fiscal_year
+    	    AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+            ON pre.customer_code = s.customer_code AND
+            pre.fiscal_year=s.fiscal_year
+	WHERE 
+	s.fiscal_year=2021     
